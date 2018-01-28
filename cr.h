853373822aace134ec52b36c5dca8a4845a85886
Rokas Kupstys <rokups@zoho.com>
2018-01-28 12:35:43 +0200

    Do not use __try/__except when compiling with MingW.

--- cr.h
+++ cr.h
@@ -922,13 +922,17 @@ static int cr_seh_filter(cr_plugin &ctx, unsigned long seh) {
 
 static int cr_plugin_main(cr_plugin &ctx, cr_op operation) {
     auto p = (cr_internal *)ctx.p;
+#ifndef __MINGW32__
     __try {
+#endif
         if (p->main) {
             return p->main(&ctx, operation);
         }
+#ifndef __MINGW32__
     } __except (cr_seh_filter(ctx, GetExceptionCode())) {
         return -1;
     }
+#endif
     return -1;
 }
 
