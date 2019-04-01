1501bf0b7493e2cf7d09fea6defd295712035add
Robert Gabriel Jakabosky <rjakabosky+github@neoawareness.com>
2019-04-02 06:24:36 +0800

    Issue with handling unload crashes. (#37)
    * Make file versions match plugin versions.
    
    Before the `new_file` was using `ctx.version` before it was incremented.
    So the number in the file name didn't match the plugin version.
    
    * Only decrement version once during crash/rollback.
    
    Both the crash handler and `cr_plugin_rollback()` where decreasing the
    version number.
    
    * Fix issues with unload crashes.
    
    Previous logic:
    1. Plugin is at version 2.
    2. New version 3 is detected.
    3. Unload of version 2 crashes.  (State is saved from version 2)
    4. Version 3 is loaded with state from version 2.
    5. Plugin is still in failed state (ctx.failure == CR_SEGFAULT)
    6. Next update does a rollback (version 3 -> 2).
    7. Plugin is back to version 2.
    
    New logic:
    1. Plugin is at version 2.
    2. New version 3 is detected.
    3. Unload of version 2 crashes.  (State is NOT saved from version 2)
    4. New version 3 is NOT loaded.  Return quickly back to host.
    5. Plugin version is still 2 in failed state. (ctx.failure == CR_SEGFAULT)
    6. Next update does a rollback (version 2 -> 1).
    7. Plugin is back to version 1.  With state restored from backup.

--- cr.h
+++ cr.h
@@ -637,7 +637,7 @@ static void cr_plugin_sections_reload(cr_plugin &ctx,
 static void cr_plugin_sections_store(cr_plugin &ctx);
 static void cr_plugin_sections_backup(cr_plugin &ctx);
 static void cr_plugin_reload(cr_plugin &ctx);
-static void cr_plugin_unload(cr_plugin &ctx, bool rollback, bool close);
+static int cr_plugin_unload(cr_plugin &ctx, bool rollback, bool close);
 static bool cr_plugin_changed(cr_plugin &ctx);
 static bool cr_plugin_rollback(cr_plugin &ctx);
 static int cr_plugin_main(cr_plugin &ctx, cr_op operation);
@@ -1617,11 +1617,15 @@ static bool cr_plugin_load_internal(cr_plugin &ctx, bool rollback) {
     auto p = (cr_internal *)ctx.p;
     const auto file = p->fullname;
     if (cr_exists(file) || rollback) {
-        const auto new_file = cr_version_path(file, ctx.version, p->temppath);
+        const auto old_file = cr_version_path(file, ctx.version, p->temppath);
+        CR_LOG("unload '%s' with rollback: %d\n", old_file.c_str(), rollback);
+        int r = cr_plugin_unload(ctx, rollback, false);
+        if (r < 0) {
+            return false;
+        }
 
-        const bool close = false;
-        CR_LOG("unload '%s' with rollback: %d\n", file.c_str(), rollback);
-        cr_plugin_unload(ctx, rollback, close);
+        auto new_version = ctx.version + (rollback ? 0 : 1);
+        const auto new_file = cr_version_path(file, new_version, p->temppath);
         if (!rollback) {
             cr_copy(file, new_file);
 
@@ -1666,7 +1670,7 @@ static bool cr_plugin_load_internal(cr_plugin &ctx, bool rollback) {
         if (ctx.failure != CR_BAD_IMAGE) {
             p2->timestamp = cr_last_write_time(file);
         }
-        ctx.version++;
+        ctx.version = new_version;
         CR_LOG("loaded: %s (version: %d)\n", new_file.c_str(), ctx.version);
     } else {
         CR_ERROR("Error loading plugin.\n");
@@ -1801,18 +1805,25 @@ static bool cr_plugin_changed(cr_plugin &ctx) {
 // unload is due a rollback, no `cr_op::CR_UNLOAD` is called neither any state
 // is saved, giving opportunity to the previous version to continue with valid
 // previous state.
-static void cr_plugin_unload(cr_plugin &ctx, bool rollback, bool close) {
+static int cr_plugin_unload(cr_plugin &ctx, bool rollback, bool close) {
     CR_TRACE
     auto p = (cr_internal *)ctx.p;
+    int r = 0;
     if (p->handle) {
         if (!rollback) {
-            cr_plugin_main(ctx, close ? CR_CLOSE : CR_UNLOAD);
-            cr_plugin_sections_store(ctx);
+            r = cr_plugin_main(ctx, close ? CR_CLOSE : CR_UNLOAD);
+            // Don't store state if unload crashed.  Rollback will use backup.
+            if (r < 0) {
+                CR_LOG("4 FAILURE: %d\n", r);
+            } else {
+                cr_plugin_sections_store(ctx);
+            }
         }
         cr_so_unload(ctx);
         p->handle = nullptr;
         p->main = nullptr;
     }
+    return r;
 }
 
 // internal
@@ -1821,7 +1832,6 @@ static void cr_plugin_unload(cr_plugin &ctx, bool rollback, bool close) {
 // in turn may also cause more rollbacks.
 static bool cr_plugin_rollback(cr_plugin &ctx) {
     CR_TRACE
-    ctx.version = ctx.version > 0 ? ctx.version - 1 : 0;
     auto loaded = cr_plugin_load_internal(ctx, true);
     if (loaded) {
         loaded = cr_plugin_main(ctx, CR_LOAD) >= 0;
